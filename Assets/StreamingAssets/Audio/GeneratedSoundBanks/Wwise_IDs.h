9e32248431d866dd822d5c74b7fe455c8f0cda27
Hamish <hamishhill1909@gmail.com>
2023-02-04 15:47:07 +0000

    Added events
    added play events for all sounds. Added sounds to soundbank

---
+++ Assets/StreamingAssets/Audio/GeneratedSoundBanks/Wwise_IDs.h
@@ -0,0 +1,53 @@
+/////////////////////////////////////////////////////////////////////////////////////////////////////
+//
+// Audiokinetic Wwise generated include file. Do not edit.
+//
+/////////////////////////////////////////////////////////////////////////////////////////////////////
+
+#ifndef __WWISE_IDS_H__
+#define __WWISE_IDS_H__
+
+#include <AK/SoundEngine/Common/AkTypes.h>
+
+namespace AK
+{
+    namespace EVENTS
+    {
+        static const AkUniqueID PLAY_AMBIENT_BIRDS_INSECTS = 1748835869U;
+        static const AkUniqueID PLAY_AMBIENT_CALM = 3522202468U;
+        static const AkUniqueID PLAY_AMBIENT_NIGHT = 894712109U;
+        static const AkUniqueID PLAY_AMBIENT_WINDY = 1791777216U;
+        static const AkUniqueID PLAY_DIGGING = 813141615U;
+        static const AkUniqueID PLAY_DIGGING_REVERB = 2589767054U;
+        static const AkUniqueID PLAY_FOOTSTEP = 1602358412U;
+        static const AkUniqueID PLAY_JUMP_LAND = 3297008698U;
+        static const AkUniqueID PLAY_JUMP_LAND_PURR = 119879040U;
+        static const AkUniqueID PLAY_JUMP_UP = 2372486210U;
+        static const AkUniqueID PLAY_MENU_BUTTON1 = 2656319675U;
+        static const AkUniqueID PLAY_MENU_BUTTON2 = 2656319672U;
+        static const AkUniqueID PLAY_MENU_BUTTON_STARTGAME = 1758056349U;
+        static const AkUniqueID PLAY_MEOW1 = 3565569787U;
+        static const AkUniqueID PLAY_MEOW2 = 3565569784U;
+        static const AkUniqueID PLAY_PURR_LOOPABLE = 477141860U;
+    } // namespace EVENTS
+
+    namespace BANKS
+    {
+        static const AkUniqueID INIT = 1355168291U;
+        static const AkUniqueID SOUNDBANK = 1661994096U;
+    } // namespace BANKS
+
+    namespace BUSSES
+    {
+        static const AkUniqueID MASTER_AUDIO_BUS = 3803692087U;
+    } // namespace BUSSES
+
+    namespace AUDIO_DEVICES
+    {
+        static const AkUniqueID NO_OUTPUT = 2317455096U;
+        static const AkUniqueID SYSTEM = 3859886410U;
+    } // namespace AUDIO_DEVICES
+
+}// namespace AK
+
+#endif // __WWISE_IDS_H__
