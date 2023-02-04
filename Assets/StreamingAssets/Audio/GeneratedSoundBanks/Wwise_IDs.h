45ff41af5829dcc7fdb92a3f7dc9ac7b9cbe8127
WILLIAM GOWER <williamgower1234@yahoo.co.uk>
2023-02-04 21:26:35 +0000

    Revert "Meow button"
    This reverts commit 4258ea4801bd0cb024a0c81bce39cb69b239f567.

--- Assets/StreamingAssets/Audio/GeneratedSoundBanks/Wwise_IDs.h
+++ Assets/StreamingAssets/Audio/GeneratedSoundBanks/Wwise_IDs.h
@@ -13,7 +13,6 @@ namespace AK
 {
     namespace EVENTS
     {
-        static const AkUniqueID MEOWCONTAINER = 1650539550U;
         static const AkUniqueID PLAY_AMBIENT_BIRDS_INSECTS = 1748835869U;
         static const AkUniqueID PLAY_AMBIENT_CALM = 3522202468U;
         static const AkUniqueID PLAY_AMBIENT_NIGHT = 894712109U;
