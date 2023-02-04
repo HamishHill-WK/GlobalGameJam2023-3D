452ea6fa94fd32c954cda27de4379db6602bd8f3
Angellliev <59960193+Angellliev@users.noreply.github.com>
2023-02-04 16:38:05 +0000

    Mineral spawner prefab

--- Assets/SpawnMineral.cs
+++ Assets/SpawnMineral.cs
@@ -29,12 +29,19 @@ public class SpawnMineral : MonoBehaviour
 
     void GenerateMinerals()
     {
+
+       // public static Object Instantiate(Object original, Vector3 position, Quaternion rotation, Transform parent);
         spawnMineralNumber = Random.Range(spawnMineralIntensity - 2, spawnMineralIntensity + 2);
         for (int i = 0; i < spawnMineralNumber; i++)
         {
-            GameObject minable = Instantiate(Mineral, this.gameObject.transform);
-            minable.transform.Rotate(new Vector3(Random.Range(0, 360), Random.Range(0, 360), Random.Range(0, 360)));
-            minable.transform.position = new Vector3(Random.Range(0, boxCollider.size.x), Random.Range(0, boxCollider.size.y), 0);
+            //Position and Rotation
+            
+            Vector3 position = new Vector3(boxCollider.transform.position.x + Random.Range(-boxCollider.size.x / 2, boxCollider.size.x / 2) * boxCollider.transform.localScale.x, boxCollider.transform.position.y + Random.Range(-boxCollider.size.y / 2, boxCollider.size.y / 2) * boxCollider.transform.localScale.y, boxCollider.transform.position.z);
+            Vector3 rotation = new Vector3(Random.Range(0, 360), Random.Range(0, 360), Random.Range(0, 360));
+            
+            GameObject minable = Instantiate(Mineral, position, new Quaternion(rotation.x, rotation.y, rotation.z, 0));
+            minable.transform.localScale = new Vector3(0.3f, 0.3f, 0.3f);
+            
         }
     }
 
